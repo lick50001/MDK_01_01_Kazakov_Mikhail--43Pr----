2f9f7bdcdee8eb68e771a21d25dd57d87b17569e
Misha <axxuetam@gmail.com>
2026-05-21 11:04:59 +0500

    Разработанная модель представления задач. Казаков

---
+++ ViewModels/VM_Tasks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Kazakov.Classes;
+using TaskManager_Kazakov.Context;
+
+namespace TaskManager_Kazakov.ViewModels
+{
+    public class VM_Tasks : Notification
+    {
+        public TasksContext tasksContext = new TasksContext();
+        public ObservableCollection<Tasks> Tasks { get; set; }
+        public VM_Tasks() =>
+            Tasks = new ObservableCollection<Tasks>(tasksContext.Tasks.OrderBy(t => t.Done));
+
+        public RealyCommand OnAddTask
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    Tasks NewTask = new Tasks()
+                    {
+                        DataExecute = DataTime.Now
+                    };
+                    Tasks.Add(NewTask);
+                    tasksContext.Tasks.Add(NewTask);
+                    tasksContext.SaveChanges();
+                })
+            }
+        }
+    }
+}
